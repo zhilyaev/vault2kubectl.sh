0ea58add9e6aada4e69713ebe055e658358f03cd
Dmitriy Zhilyaev <zhilyaev@users.noreply.github.com>
2022-12-27 22:19:43 +0300

    Update vault2kubectl.sh

--- vault2kubectl.sh
+++ vault2kubectl.sh
@@ -18,16 +18,26 @@ if [ "$DEBUG" == "1" ]; then
 fi
 
 
+
 # DATA
 data=$(vault kv get -format=json "$VAULT_PATH/$cluster" | jq '.data.data')
-certificate_authority=$( echo $data | jq '.certificate_authority' | base64)
-client_certificate=$( echo $data | jq '.client_certificate' | base64)
-client_key=$( echo $data | jq '.client_key' | base64)
-server=$( echo $data | jq '.server')
+
+certificate_authority=$(echo $data | jq -r '.certificate_authority' | base64)
+client_certificate=$(echo $data | jq -r '.client_certificate' | base64)
+client_key=$(echo $data | jq -r '.client_key' | base64)
+server=$(echo $data | jq -r '.server')
+
+if [ "$DEBUG" == "1" ]; then
+  echo "$server"
+fi
+
 
 
 # SETUP
-kubectl config set-cluster "$cluster" --server="$server"
+kubectl config set-cluster "$cluster" --server "$server"
 kubectl config set "clusters.$cluster.certificate-authority-data" "$certificate_authority"
 kubectl config set "users.kubernetes-admin@$cluster.client-certificate-data" "$client_certificate"
 kubectl config set "users.kubernetes-admin@$cluster.client-key-data" "$client_key"
+kubectl config set-context "$cluster" --cluster="$cluster" --user="kubernetes-admin@$cluster"
+kubectl config use-context "$cluster"
+# kubectl get ns
